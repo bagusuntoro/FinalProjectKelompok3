47765763d9cb7a3552ee69268c85f26e21d27260
novifitri <vii.novi039@gmail.com>
2022-11-09 14:11:01 +0700

    modify create with service repository pattern

--- app/Http/Services/InstructionService.php
+++ app/Http/Services/InstructionService.php
@@ -3,6 +3,9 @@
 namespace App\Http\Services;
 
 use App\Http\Repositories\InstructionRepository;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class InstructionService
 {
@@ -30,4 +33,53 @@ class InstructionService
         return $instruction;
 
     }
+
+    public function create($request)
+    {
+         $validator = Validator::make($request, [
+            'instruction_id' => 'required',
+            'link_to' => 'required',
+            'instruction_type' => 'required',
+            'assigned_vendor' => 'required',
+            'attention_of' => 'required',
+            'quotation_no' => 'required',
+            'customer_po' => 'required',
+            'note' => 'required',
+            'link_to' => 'required',
+            'attachment' => 'mimes:pdf,zip',
+        ]);
+         //jika validasi gagal
+        if($validator->fails())
+        {
+            throw new InvalidArgumentException($validator->errors());
+        }
+
+        // //jika validasi berhasil 
+        $detail_cost = $this->insertMultipleCostDetail($request);
+        $user = auth()->user()->name;
+        $request['detail_cost'] = $detail_cost;
+        $request['user'] = $user;
+        $instruction= $this->instructionRepository->create($request);
+        $data = $this->instructionRepository->getById($instruction);
+		return $data;
+    }
+    protected function insertMultipleCostDetail($request)
+    {
+        $data = [];
+        $detail = [];
+        for ($i = 1; $i <= count($request["cost_detail"]); $i++) {
+            $data["_id"] = $i;
+            $data["description"] = $request['cost_detail']["detail$i"]["description"];
+            $data["qty"] = $request['cost_detail']["detail$i"]["qty"];
+            $data["uom"] = $request['cost_detail']["detail$i"]["uom"];
+            $data["unit_price"] = $request['cost_detail']["detail$i"]["unit_price"];
+            $data["gst_vat"] = $request['cost_detail']["detail$i"]["gst_vat"];
+            $data["currency"] = $request['cost_detail']["detail$i"]["currency"];
+            $data["total"] = $request['cost_detail']["detail$i"]["total"];
+            $data["charge_to"] = $request['cost_detail']["detail$i"]["charge_to"];
+            array_push($detail, $data);
+        }
+        return $detail;
+    }
+
 }
