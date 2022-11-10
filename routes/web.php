6cf94ac03724d33e3e7bf52ffd00377f018e26e7
bagusuntoro <untorobagus65@gmail.com>
2022-11-10 12:17:05 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function(){
     return view('home');
 });
+
+// Route::get('/home', function(){
+//     return view('welcome');
+// });
